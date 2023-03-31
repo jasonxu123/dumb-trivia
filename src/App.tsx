c128ada382e1c0a2c0216fac83fe8508260a2e32
Jason Xu <triviadumb@gmail.com>
2023-03-31 03:17:53 -0700

    try out mui new styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
+import { Button, Icon, styled } from '@mui/material';
+import { Box } from '@mui/system';
 import React from 'react';
 
 // TODO: use process.env.NODE_ENV for production or dev API routes
 
-export const App = () => {
-  return (
-    <div>
-      <h2>Santa Claus says</h2>
-      <div style={{ color: 'blueviolet', fontSize: '14px' }}>
-        Merry Christmas
-      </div>
-      <div>{process.env.NODE_ENV}</div>
-    </div>
-  );
-};
+const GlobalWrapper = styled('div')(
+  ({ theme }) => `
+  color: ${theme.palette.text.primary};
+  font-family: ${theme.typography.fontFamily};
+`,
+);
+
+const ButtonRow = styled('div')(`
+  padding: 16px;
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+  align-items: center;
+`);
+
+export const App = () => (
+  <GlobalWrapper>
+    <Box sx={{ color: 'text.primary', fontSize: 34, fontWeight: 'medium' }}>
+      Test that sheets work
+    </Box>
+    <ButtonRow>
+      <Icon>star</Icon>
+      <Button variant="contained" disableElevation>
+        Test the thing
+      </Button>
+    </ButtonRow>
+    <div>{process.env.NODE_ENV}</div>
+  </GlobalWrapper>
+);
